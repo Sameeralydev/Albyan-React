7648e54eb52bcced6d5565d66a24c6b066f97dd6
Sameer <sameeraly2003@gmail.com>
2025-12-23 11:05:15 +0500

    fixed

--- src/layout/navbar/StickyNavbar.jsx
+++ src/layout/navbar/StickyNavbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import navbarLinks from "../../Data/navbar";
+import navbarLinks from "../../data/navbar";
 
 export default function StickyNavbar() {
   const [show, setShow] = useState(false);
